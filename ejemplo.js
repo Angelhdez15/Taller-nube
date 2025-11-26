9e290c0140fcb5d0b5ea922e02611d5f0882650a
Luis Hernandez <luisangelhernandezflores604@gmail.com>
2025-11-26 07:21:56 -1200

    Add subtraction function to ejemplo.js
    Added subtraction function and updated console logs.

--- ejemplo.js
+++ ejemplo.js
@@ -1,4 +1,12 @@
+//funcion para sumar
 function sumar(a, b) {
     return a+b;
 }
 console.log(sumar(2, 4)); 
+
+//funcion para restar
+function sumar(a, b) {
+    return a-b;
+}
+console.log(sumar(4, 2)); 
+
